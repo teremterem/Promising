de5c968247295e5a262e6824596e47e7e80a0b05
Oleksandr Tereshchenko <toporok@gmail.com>
2025-08-13 00:18:46 +0300

    make test_as_concurrent_future much more sophisticated - test all configurations of the promise

--- tests/promises/test_concurrent_future.py
+++ tests/promises/test_concurrent_future.py
@@ -5,34 +5,61 @@ Simple test script to verify the as_concurrent_future() method works.
 import asyncio
 import concurrent.futures
 import threading
+from typing import Optional
 import pytest
 from promising.promises import Promise
 
 
-async def test_as_concurrent_future():
+@pytest.mark.parametrize("start_soon", [True, False, None])
+@pytest.mark.parametrize("await_promise", [True, False, None])
+@pytest.mark.parametrize("get_future_before_await", [True, False])
+async def test_as_concurrent_future(
+    start_soon: Optional[bool],
+    await_promise: Optional[bool],
+    get_future_before_await: bool,
+):
     """
     Test Promise.as_concurrent_future().
     """
 
-    async def sample_coro():
-        await asyncio.sleep(0.1)
-        return "Hello from Promise!"
-
     # Create a Promise
-    promise = Promise(sample_coro())
+    if start_soon is None:
+        # `start_soon=None` in our test means that we want to create a prefilled promise
+        promise = Promise(prefill_result="Hello from Promise!")
+    else:
 
-    # Convert to concurrent.futures.Future
-    concurrent_future = promise.as_concurrent_future()
+        async def sample_coro():
+            await asyncio.sleep(0.1)
+            return "Hello from Promise!"
 
-    # Test that it's the right type
-    assert isinstance(concurrent_future, concurrent.futures.Future)
+        promise = Promise(sample_coro(), start_soon=start_soon)
 
-    # Wait for completion
-    await promise
+    if get_future_before_await:
+        # Get the concurrent future before we await for anything
+        concurrent_future = promise.as_concurrent_future()
 
-    # Check that the concurrent future also completed
-    assert concurrent_future.done()
-    assert concurrent_future.result() == "Hello from Promise!"
+    if await_promise is True:
+        await promise
+    elif await_promise is False:
+        # Let's await in general, but not for the promise specifically
+        await asyncio.sleep(0.2)
+    # `await_promise=None` in our test means that we don't want to await for anything at all (no task switching)
+
+    if not get_future_before_await:
+        # Get the concurrent future after we await for anything
+        concurrent_future = promise.as_concurrent_future()
+
+    assert isinstance(concurrent_future, concurrent.futures.Future)  # pylint: disable=possibly-used-before-assignment
+
+    if (start_soon is not None and await_promise is None) or (start_soon is False and await_promise is not True):
+        # Two scenarios when the promise is not expected to be done:
+        # 1. The promise is not prefilled and we don't await for anything at all (no task switching happens)
+        # 2. The promise does not start soon (and is not prefilled), but we don't await for it directly
+        assert not concurrent_future.done()
+    else:
+        # In all other scenarios the promise should be done
+        assert concurrent_future.done()
+        assert concurrent_future.result() == "Hello from Promise!"
 
 
 async def test_with_exception():

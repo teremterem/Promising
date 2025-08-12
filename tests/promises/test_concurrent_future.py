8ace81717a733849d8ddd5ebe6e4e0bc34ea396d
Oleksandr Tereshchenko <toporok@gmail.com>
2025-08-13 00:44:18 +0300

    test_with_exception is now more sophisticated

--- tests/promises/test_concurrent_future.py
+++ tests/promises/test_concurrent_future.py
@@ -62,25 +62,59 @@ async def test_as_concurrent_future(
         assert concurrent_future.result() == "Hello from Promise!"
 
 
-async def test_with_exception():
+@pytest.mark.parametrize("start_soon", [True, False, None])
+@pytest.mark.parametrize("await_promise", [True, False, None])
+@pytest.mark.parametrize("get_future_before_await", [True, False])
+async def test_with_exception(
+    start_soon: Optional[bool],
+    await_promise: Optional[bool],
+    get_future_before_await: bool,
+):
     """
     Test Promise.as_concurrent_future() with exceptions.
     """
 
-    async def failing_coro():
-        await asyncio.sleep(0.1)
-        raise ValueError("Test error")
+    # Create a Promise
+    if start_soon is None:
+        # `start_soon=None` in our test means that we want to create a prefilled promise with exception
+        promise = Promise(prefill_exception=ValueError("Test error from Promise!"))
+    else:
 
-    promise = Promise(failing_coro())
-    concurrent_future = promise.as_concurrent_future()
+        async def failing_coro():
+            await asyncio.sleep(0.1)
+            raise ValueError("Test error from Promise!")
 
-    with pytest.raises(ValueError):
-        await promise
+        promise = Promise(failing_coro(), start_soon=start_soon)
 
-    assert concurrent_future.done()
-    with pytest.raises(ValueError) as exc_info:
-        concurrent_future.result()
-    assert str(exc_info.value) == "Test error"
+    if get_future_before_await:
+        # Get the concurrent future before we await for anything
+        concurrent_future = promise.as_concurrent_future()
+
+    if await_promise is True:
+        with pytest.raises(ValueError):
+            await promise
+    elif await_promise is False:
+        # Let's await in general, but not for the promise specifically
+        await asyncio.sleep(0.2)
+    # `await_promise=None` in our test means that we don't want to await for anything at all (no task switching)
+
+    if not get_future_before_await:
+        # Get the concurrent future after we await for anything
+        concurrent_future = promise.as_concurrent_future()
+
+    assert isinstance(concurrent_future, concurrent.futures.Future)  # pylint: disable=possibly-used-before-assignment
+
+    if (start_soon is not None and await_promise is None) or (start_soon is False and await_promise is not True):
+        # Two scenarios when the promise is not expected to be done:
+        # 1. The promise is not prefilled and we don't await for anything at all (no task switching happens)
+        # 2. The promise does not start soon (and is not prefilled), but we don't await for it directly
+        assert not concurrent_future.done()
+    else:
+        # In all other scenarios the promise should be done (with exception)
+        assert concurrent_future.done()
+        with pytest.raises(ValueError) as exc_info:
+            concurrent_future.result()
+        assert str(exc_info.value) == "Test error from Promise!"
 
 
 async def test_from_thread():

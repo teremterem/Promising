3ca69165f0ea80deb890146d5e4c799bf4c8202a
Oleksandr Tereshchenko <toporok@gmail.com>
2025-08-24 23:33:13 +0300

    fix ready todos

--- tests/promises/test_concurrent_future.py
+++ tests/promises/test_concurrent_future.py
@@ -10,6 +10,64 @@ import pytest
 from promising.promises import Promise
 
 
+def _get_future_result(container, index, future, timeout):
+    try:
+        container[index] = future.result(timeout=timeout)
+    except concurrent.futures.TimeoutError as e:
+        container[index] = e
+
+
+async def _run_thread_test(start_soon: Optional[bool], await_promise: Optional[bool]) -> None:
+    calls = 0
+    if start_soon is None:
+        promise = Promise(prefill_result="Result from thread test!")
+    else:
+
+        async def sample_coro():
+            nonlocal calls
+            calls += 1
+            await asyncio.sleep(0.2)
+            return "Result from thread test!"
+
+        promise = Promise(sample_coro(), start_soon=start_soon)
+
+    concurrent_future = promise.as_concurrent_future()
+
+    results = [None, None, None]
+    thread1 = threading.Thread(target=_get_future_result, args=(results, 0, concurrent_future, 0.4))
+    thread2 = threading.Thread(target=_get_future_result, args=(results, 1, concurrent_future, 0.4))
+    thread3 = threading.Thread(target=_get_future_result, args=(results, 2, concurrent_future, 0.1))
+
+    thread1.start()
+    thread2.start()
+    thread3.start()
+
+    if await_promise is True:
+        await promise
+    elif await_promise is False:
+        await asyncio.sleep(0.3)
+
+    thread1.join()
+    thread2.join()
+    thread3.join()
+
+    if (start_soon is not None and await_promise is None) or (start_soon is False and await_promise is not True):
+        assert isinstance(results[0], concurrent.futures.TimeoutError)
+        assert isinstance(results[1], concurrent.futures.TimeoutError)
+        assert isinstance(results[2], concurrent.futures.TimeoutError)
+        await promise
+    else:
+        assert results[0] == "Result from thread test!"
+        assert results[1] == "Result from thread test!"
+        if start_soon is None:
+            assert results[2] == "Result from thread test!"
+        else:
+            assert isinstance(results[2], concurrent.futures.TimeoutError)
+
+    if start_soon is not None:
+        assert calls == 1
+
+
 @pytest.mark.parametrize("start_soon", [True, False, None])
 @pytest.mark.parametrize("await_promise", [True, False, None])
 @pytest.mark.parametrize("get_future_before_await", [True, False])
@@ -134,88 +192,4 @@ async def test_from_threads(
     start_soon: Optional[bool],
     await_promise: Optional[bool],
 ):
-    """
-    Test accessing Promise result from different threads.
-    """
-
-    # Create a Promise
-    if start_soon is None:
-        # `start_soon=None` in our test means that we want to create a prefilled promise
-        promise = Promise(prefill_result="Result from thread test!")
-    else:
-
-        async def sample_coro():
-            await asyncio.sleep(0.2)
-            return "Result from thread test!"
-
-        promise = Promise(sample_coro(), start_soon=start_soon)
-
-    concurrent_future = promise.as_concurrent_future()
-
-    result1 = None
-    result2 = None
-    result3 = None
-
-    def thread_function1():
-        nonlocal result1
-        try:
-            result1 = concurrent_future.result(timeout=0.4)
-        except concurrent.futures.TimeoutError as e:
-            result1 = e
-
-    def thread_function2():
-        nonlocal result2
-        try:
-            result2 = concurrent_future.result(timeout=0.4)
-        except concurrent.futures.TimeoutError as e:
-            result2 = e
-
-    def thread_function3():
-        nonlocal result3
-        try:
-            # Time out earlier than the promise is completed
-            result3 = concurrent_future.result(timeout=0.1)
-        except concurrent.futures.TimeoutError as e:
-            result3 = e
-
-    thread1 = threading.Thread(target=thread_function1)
-    thread2 = threading.Thread(target=thread_function2)
-    thread3 = threading.Thread(target=thread_function3)
-    thread1.start()
-    thread2.start()
-    thread3.start()
-
-    if await_promise is True:
-        await promise
-    elif await_promise is False:
-        # Let's await in general, but not for the promise specifically
-        await asyncio.sleep(0.3)
-    # `await_promise=None` in our test means that we don't want to await for anything at all (no task switching)
-
-    thread1.join()
-    thread2.join()
-    thread3.join()
-
-    if (start_soon is not None and await_promise is None) or (start_soon is False and await_promise is not True):
-        # Two scenarios when the promise is not expected to be done:
-        # 1. The promise is not prefilled and we don't await for anything at all (no task switching happens)
-        # 2. The promise does not start soon (and is not prefilled), but we don't await for it directly
-        assert isinstance(result1, concurrent.futures.TimeoutError)
-        assert isinstance(result2, concurrent.futures.TimeoutError)
-        assert isinstance(result3, concurrent.futures.TimeoutError)
-
-        # Now, that we ensured that concurrent_future is not done no matter the waiting time out, let's await for the
-        # promise directly, so we don't get the asyncio warning about it never being awaited
-        await promise
-
-    else:
-        assert result1 == "Result from thread test!"
-        assert result2 == "Result from thread test!"
-        if start_soon is None:
-            # The promise was prefilled, so the result should be available even for the thread that did not wait long
-            # enough
-            assert result3 == "Result from thread test!"
-        else:
-            assert isinstance(result3, concurrent.futures.TimeoutError)
-
-    # TODO [READY] Assert that the sample_coro() is called exactly once in this test method
+    await _run_thread_test(start_soon, await_promise)

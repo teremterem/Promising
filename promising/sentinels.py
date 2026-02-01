0751e0892bab8890269f91318adbae8027fbe729
Oleksandr Tereshchenko <toporok@gmail.com>
2026-02-02 01:23:29 +0200

    format docstrings and comments

--- promising/sentinels.py
+++ promising/sentinels.py
@@ -1,7 +1,8 @@
 class Sentinel:
     def __bool__(self) -> bool:
-        # TODO Should any other magic methods be defined like this to prevent reliance on Sentinel's truthiness ?
-        #  We need to come up with a list of such magic methods if we want to take care of this TODO.
+        # TODO Should any other magic methods be defined like this to prevent
+        #  reliance on Sentinel's truthiness ? We need to come up with a list
+        #  of such magic methods if we want to take care of this TODO.
         raise RuntimeError("Sentinels should not be used in boolean expressions.")
 
 

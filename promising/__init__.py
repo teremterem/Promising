2fe05032743aeffe02abaeb334e6caa660ccf9d1
Oleksandr Tereshchenko <toporok@gmail.com>
2026-02-11 21:41:51 +0200

    in progress: PromisingFunction

--- promising/__init__.py
+++ promising/__init__.py
@@ -1,11 +1,9 @@
-from promising.backends import PromisingBackend
 from promising.config import PromisingConfig
 from promising.promise import Promise, get_current_promise
 from promising.promising_function import PromisingFunction
 
 __all__ = [
     "Promise",
-    "PromisingBackend",
     "PromisingConfig",
     "PromisingFunction",
     "get_current_promise",

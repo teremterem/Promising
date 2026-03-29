d0f03bbdfeedf6ed1aae0cd4bc946fb0b87a542c
Oleksandr Tereshchenko <toporok@gmail.com>
2026-03-29 23:32:23 +0300

    use AI again: rename some tests

--- tests/configuration/test_call_overrides_sync.py
+++ tests/configuration/test_call_overrides_sync.py
@@ -24,11 +24,12 @@ async def test_config_params_work_with_sync_functions() -> None:
     await promise
 
 
-async def test_call_time_config_overrides_work_with_sync_functions() -> None:
+async def test_call_time_config_overrides() -> None:
     """
     Config params passed at call time override the
-    PromisingFunction-level defaults for sync functions.
+    PromisingFunction-level defaults.
     """
+    # TODO How not to forget to expand the list of params when they are added ?
 
     @promising.function(
         start_soon=False,
